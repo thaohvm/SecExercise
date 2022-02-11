cb88594fac93738fd943d15ae807cc8c21492f95
Thao Huynh <thaohvm@gmail.com>
2022-02-10 21:09:03 -0800

    update router.get name

--- Unit31/express-shopping-list/itemRoutes.js
+++ Unit31/express-shopping-list/itemRoutes.js
@@ -10,28 +10,29 @@ router.get('/', (req, res) => {
     res.json({ items: items })
 })
 
-router.post('/', middleware.validateData, (req,res, next) => {
+router.post('/', middleware.validateData, (req, res, next) => {
     try {
         if (req.body.name == items.name) {
             throw new ExpressError("Items have been existed", 404)
         }
         items.push(req.body);
         res.json({ added: req.body })
-    } catch(e) {
+    } catch (e) {
         return next(e);
     }
 });
 
 router.get('/:name', (req, res, next) => {
-    const requestedName = res.params.name;
+    const requestedItem = items.find(item => item.name === req.params.name);
     try {
-        if (Object.values(items).indexOf(req.params.name) === -1) {
-            throw new ExpressError("Item is not existed", 404)
-        }
-        return res.status(200).json(items[requestedName]);
+        if (!requestedItem) {
+            throw new ExpressError("Item is not exist!", 404);
+        } else {
+        return res.json({ requestedItem })
+        };
     } catch(e) {
-        return next(e);
-    };
+        return next(e)
+    }
 });
 
 module.exports = router;
