581ef88e4ada9b4f30831cfdbc9037b550510c06
Thao Huynh <thaohvm@gmail.com>
2022-02-10 21:35:47 -0800

    update router.patch

--- Unit31/express-shopping-list/itemRoutes.js
+++ Unit31/express-shopping-list/itemRoutes.js
@@ -3,6 +3,7 @@ const router = new express.Router();
 const items = require("./fakeDb");
 const ExpressError = require("./expressError")
 const middleware = require("./middleware");
+const { request } = require("express");
 
 // const ITEMS = [{"name": "popsicle", "price": 1.45}, {"name":"cheerios", "price": 3.40}]
 
@@ -12,7 +13,7 @@ router.get('/', (req, res) => {
 
 router.post('/', middleware.validateData, (req, res, next) => {
     try {
-        if (req.body.name == items.name) {
+        if (req.body.name === items.name) {
             throw new ExpressError("Items have been existed", 404)
         }
         items.push(req.body);
@@ -28,7 +29,7 @@ router.get('/:name', (req, res, next) => {
         if (!requestedItem) {
             throw new ExpressError("Item is not exist!", 404);
         } else {
-        return res.json({ requestedItem })
+            return res.json({ requestedItem })
         };
     } catch(e) {
         return next(e)
@@ -36,3 +37,18 @@ router.get('/:name', (req, res, next) => {
 });
 
 module.exports = router;
+
+router.patch('/:name', (req, res, next) => {
+    let requestedItem = items.find(item => item.name === req.params.name);
+    try {
+        if (!requestedItem) {
+            throw new ExpressError("Item is not exist!", 404);
+        } else {
+            requestedItem.name = req.body.name;
+            requestedItem.price = req.body.price;
+            return res.json
+        }
+    } catch(e) {
+        return next(e)
+    }
+})
