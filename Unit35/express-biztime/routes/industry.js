9959027cf7c43f4952845102d404baf966045242
Thao Huynh <thaohvm@gmail.com>
2022-02-18 12:19:07 -0800

    update industry routing

--- Unit35/express-biztime/routes/industry.js
+++ Unit35/express-biztime/routes/industry.js
@@ -3,3 +3,33 @@ const express = require('express');
 const router = new express.Router();
 const ExpressError = require('../expressError');
 const db = require('../db');
+
+router.get('/', async (req,res,next) => {
+    try {
+        const industryRes = await db.query(`
+            SELECT *
+            FROM industries`);
+
+        const industryPromises = industryRes.rows.map(async (industry) => {
+            const companies = await db.query(`
+                SELECT comp_code
+                FROM company_industries
+                JOIN industries
+                ON company_industries.ind_code = industries.code
+                WHERE ind_code = $1`,[industry.code]
+            );
+            const compCodes = companies.rows.map(codeObj => {
+                return codeObj.comp_code;
+            })
+            industry.companies = compCodes;
+            return industry;
+        });
+        const industries = await Promise.all(industryPromises);
+        console.log(industries);
+        return res.status(200).json({industries: industries})
+    } catch(err) {
+        return next(err);
+    };
+});
+
+module.exports = router;
