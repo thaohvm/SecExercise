530e61cc5a28ad5cc6db60ff70a7f6ed5854eaef
Thao Huynh <thaohvm@gmail.com>
2022-02-24 16:08:59 -0800

    adding validation

--- Unit36/express-bookstore/config.js
+++ Unit36/express-bookstore/config.js
@@ -4,9 +4,9 @@
 let DB_URI = `postgresql://`;
 
 if (process.env.NODE_ENV === "test") {
-  DB_URI = `${DB_URI}/books-test`;
+  DB_URI = `${DB_URI}/bookstore-test`;
 } else {
-  DB_URI = process.env.DATABASE_URL || `${DB_URI}/books`;
+  DB_URI = process.env.DATABASE_URL || `${DB_URI}/bookstore`;
 }
 
 
