ee98f279d891d6fd20ec43f31ab8a2f8557cda16
Thao Huynh <thaohvm@gmail.com>
2022-03-12 00:59:41 -0800

    update index

---
+++ Unit39/ES2015/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { choice, remove } from './helpers';
+import fruits from './foods'
+
+let fruit = choice(fruits);
+console.log(`I'd like one ${fruit}, please`)
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+
+let remaining = remove(fruit, fruits);
+console.log(`I'm sorry, we're all out. We have ${remaining.length} left.`);
