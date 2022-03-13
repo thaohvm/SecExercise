f8d23b8a25b4e0852d3907eb3aeace573d416b7e
Thao Huynh <thaohvm@gmail.com>
2022-03-12 23:54:59 -0800

    update pokegame

--- Unit39/pokedex/src/App.js
+++ Unit39/pokedex/src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import Pokedex from './Pokedex';
+import React from "react";
+import Pokegame from "./Pokegame";
 
-function App () {
-    return (
-        <div className="App">
-            <Pokedex />
-        </div>
-    );
+function App() {
+  return (
+    <div className="App">
+      <Pokegame />
+    </div>
+  );
 }
 
 export default App;
