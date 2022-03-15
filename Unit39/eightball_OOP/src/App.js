503835c239049d02f27b1626edcd707df574923c
Thao Huynh <thaohvm@gmail.com>
2022-03-15 11:11:01 -0700

    update eightball oop

---
+++ Unit39/eightball_OOP/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import EightBall from "./EightBall"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <EightBall />
+    </div>
+  );
+}
+
+export default App;
