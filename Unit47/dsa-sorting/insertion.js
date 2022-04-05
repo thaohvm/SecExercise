f07aacd8c87fdefda36db6b8375d6d3a030a7f6c
Thao Huynh <thaohvm@gmail.com>
2022-04-04 22:43:28 -0700

    update merge sort

--- Unit47/dsa-sorting/insertion.js
+++ Unit47/dsa-sorting/insertion.js
@@ -1,3 +1,14 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
+            j -= 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
 
 module.exports = insertionSort;
--- Unit47/dsa-sorting/merge.js
+++ Unit47/dsa-sorting/merge.js
@@ -1,5 +1,35 @@
-function merge() {}
+function merge(arr1, arr2) {
+    let result = [];
+    let i = 0;
+    let j = 0;
 
-function mergeSort() {}
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            result.push(arr1[i]);
+            i++;
+        } else {
+            result.push(arr2[j]);
+            j++;
+        }
+    }
+    while (i < arr1.length) {
+        result.push(arr1[i]);
+        i++;
+    }
+    while (j < arr2.length) {
+        result.push(arr2[j]);
+        j++;
+    }
+    return result;
+}
 
-module.exports = { merge, mergeSort};
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+    const mid = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+
+    return merge(left, right)
+}
+
+module.exports = { merge, mergeSort };
