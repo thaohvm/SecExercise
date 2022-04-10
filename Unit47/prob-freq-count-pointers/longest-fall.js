0b775226727291c651ecb921a8b5760d443fbd0e
Thao Huynh <thaohvm@gmail.com>
2022-04-10 00:45:38 -0700

    update further study

--- Unit47/prob-freq-count-pointers/longest-fall.js
+++ Unit47/prob-freq-count-pointers/longest-fall.js
@@ -1,3 +1,17 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(arr) {
+    let longest = 0;
+    let current = 1;
+    if (arr.length === 0) return 0;
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            current++;
+            if (current > longest) {
+                longest = current;
+            }
+        } else {
+          current = 1;
+        }
+    }
+    return longest || 1;
 }
