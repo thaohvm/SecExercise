d7d8f39bdf7e779b69780a2de6464088f57b3e7a
Thao Huynh <thaohvm@gmail.com>
2022-04-14 21:57:41 -0700

    update guessing-game

--- Unit48/outcomes-js-tricky-parts/guessing-game.js
+++ Unit48/outcomes-js-tricky-parts/guessing-game.js
@@ -1,5 +1,22 @@
 function guessingGame() {
+    const num = Math.floor(Math.random() * 100);
+    let counter = 0;
+    let isOver = false;
 
+    return function guessNum(guess) {
+        if (isOver) return "The game is over, you already won!"
+        if (guess === num) {
+            counter++;
+            isOver = true;
+            return `You win! You found ${num} in ${counter} guesses.`
+        } else if (guess < num) {
+            counter++;
+            return `${guess} is too low!`
+        } else {
+            counter++;
+            return `${guess} is too high!`
+        }
+    }
 }
 
 module.exports = { guessingGame };
