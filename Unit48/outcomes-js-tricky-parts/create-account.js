cd11af689d671b8c54b8fc47b91b5c4e7cfae838
Thao Huynh <thaohvm@gmail.com>
2022-04-14 22:34:01 -0700

    update create-account

--- Unit48/outcomes-js-tricky-parts/create-account.js
+++ Unit48/outcomes-js-tricky-parts/create-account.js
@@ -1,5 +1,46 @@
-function createAccount(pin, amount) {
+function createAccount(pin, amount = 0) {
+    return {
+        checkBalance : function(inputPin) {
+            // check balance
+            if (inputPin !== pin) {
+                return "Invalid PIN."
+            } else {
+                return `$${amount}`;
+            }
+        },
 
+        deposit : function(inputPin, depositAmount) {
+            //deposit
+            if (inputPin === pin && depositAmount) {
+                amount += depositAmount;
+                return `Succesfully deposited $${depositAmount}. Current balance: $${amount}.`
+            } else {
+                return "Invalid PIN."
+            }
+        },
+        withdraw : function(inputPin, withdrawAmount) {
+            //withdraw
+            if (inputPin === pin && withdrawAmount) {
+                if (amount > withdrawAmount) {
+                    amount -= withdrawAmount;
+                return `Succesfully withdrew $${withdrawAmount}. Current balance: $${amount}.`
+                } else {
+                    return "Withdrawal amount exceeds account balance. Transaction cancelled."
+                }
+            } else {
+                return "Invalid PIN."
+            }
+        },
+        changePin : function(oldPin, newPin) {
+            //change pin
+            if (oldPin === pin) {
+                pin = newPin;
+                return "PIN successfully changed!"
+            } else {
+                return "Invalid PIN."
+            }
+        },
+    }
 }
 
 module.exports = { createAccount };
