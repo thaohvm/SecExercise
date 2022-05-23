81e49119ffb94761c7e6e87f85fcc5adfe5ba937
Thao Huynh <thaohvm@gmail.com>
2022-05-23 01:28:52 -0700

    update class Joke

--- Unit41/react-jokes-classes/src/App.js
+++ Unit41/react-jokes-classes/src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import JokeList from "./JokeList";
 
-function App() {
-  return (
-    <div className="App">
-      <JokeList />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <JokeList />
+      </div>
+    );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <JokeList />
+//     </div>
+//   );
+// }
 
 export default App;
