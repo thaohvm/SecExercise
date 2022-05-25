864939a0bd1a43c2ed35dd19693606a71a20e32b
Thao Huynh <thaohvm@gmail.com>
2022-05-25 11:33:51 -0700

    update bind(this)

--- Unit41/react-jokes-classes/src/JokeList.js
+++ Unit41/react-jokes-classes/src/JokeList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Component } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
